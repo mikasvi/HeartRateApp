b29e04fb4e247dd3c148a19a8aaac82505e34825
Mika Sarajärvi <mika.sarajarvi@gmail.com>
2023-08-29 19:16:09 +0300

    commit 1

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [age, setAge] = useState('');
+  const [lowerLimit, setLowerLimit] = useState(0);
+  const [upperLimit, setUpperLimit] = useState(0);
+
+  // Function to calculate heart rate limits
+  const calculateHeartRateLimits = (ageValue) => {
+    const ageNum = parseInt(ageValue, 10);
+    if (isNaN(ageNum)) {
+      setLowerLimit(0);
+      setUpperLimit(0);
+      return;
+    }
+    const lower = (220 - ageNum) * 0.65;
+    const upper = (220 - ageNum) * 0.85;
+    setLowerLimit(Math.floor(lower));
+    setUpperLimit(Math.floor(upper));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Heart Rate Limits Calculator</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your age"
+        keyboardType="numeric"
+        value={age}
+        onChangeText={(text) => {
+          setAge(text);
+          calculateHeartRateLimits(text);
+        }}
+      />
+      <Text style={styles.result}>
+        Lower Limit: {lowerLimit}
+      </Text>
+      <Text style={styles.result}>
+        Upper Limit: {upperLimit}
+      </Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+  result: {
+    fontSize: 18,
   },
 });
+
+export default App;
